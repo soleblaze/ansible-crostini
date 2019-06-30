44d2fa3400e1e8b114159495eb241cf2556a0fac
Kevin B <soleblaze@skyshadows.net>
2019-06-30 11:54:47 -0500

    Add update step

--- initial.sh
+++ initial.sh
@@ -5,6 +5,7 @@ cd ~/git || exit
 git clone git@github.com:soleblaze/ansible-crostini.git
 cd ansible-crostini
 git checkout personal
+sudo apt-get update
 sudo apt-get -y install ansible
 ansible-playbook first.yml
 sudo systemctl restart sshd
