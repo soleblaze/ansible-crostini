952d33d612e9a65392d026ffcde6d06ecde42621
Kevin B <soleblaze@skyshadows.net>
2019-06-30 11:53:58 -0500

    Add missing steps

--- initial.sh
+++ initial.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+mkdir -p ~/git/
+cd ~/git || exit
+git clone git@github.com:soleblaze/ansible-crostini.git
+cd ansible-crostini
+git checkout personal
 sudo apt-get -y install ansible
 ansible-playbook first.yml
+sudo systemctl restart sshd
