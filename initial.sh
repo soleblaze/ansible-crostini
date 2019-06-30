af93be06250261412700c43afabfd7ef9331f3dc
Kevin B <soleblaze@skyshadows.net>
2019-06-30 11:56:32 -0500

    Change git repo url to https

--- initial.sh
+++ initial.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ~/git/
 cd ~/git || exit
-git clone git@github.com:soleblaze/ansible-crostini.git
-cd ansible-crostini
+git clone https://github.com/soleblaze/ansible-crostini.git
+cd ansible-crostini || exit
 git checkout personal
 sudo apt-get update
 sudo apt-get -y install ansible
